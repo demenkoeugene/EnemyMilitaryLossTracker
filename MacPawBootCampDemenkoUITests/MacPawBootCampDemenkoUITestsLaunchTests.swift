255ceaa0c04c6068d2b3536bfa4416165ed2c82b
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-08-21 22:41:00 +0300

    Initial Commit

---
+++ MacPawBootCampDemenkoUITests/MacPawBootCampDemenkoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MacPawBootCampDemenkoUITestsLaunchTests.swift
+//  MacPawBootCampDemenkoUITests
+//
+//  Created by Eugene Demenko on 21.08.2023.
+//
+
+import XCTest
+
+final class MacPawBootCampDemenkoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
