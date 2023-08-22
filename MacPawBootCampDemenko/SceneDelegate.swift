11ce7422dfd5973f0f6c4ab4e33471c638d4ed7f
demenkoeugene <mimosa.01-purgers@icloud.com>
2023-08-22 18:01:11 +0300

    fix bugs

--- MacPawBootCampDemenko/SceneDelegate.swift
+++ MacPawBootCampDemenko/SceneDelegate.swift
@@ -14,12 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        let viewController = TabbarController()
-        let navigationController = UINavigationController(rootViewController: viewController)
-        
+      
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = navigationController
+        window.rootViewController = TabbarController()
         self.window = window
         
         window.makeKeyAndVisible()
